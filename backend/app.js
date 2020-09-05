9ef3df5298dc9d45abc9a0ff4b27302e5021009f
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-09-06 00:56:49 +0300

    set express-validator as middleware

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+
 // import routes
 const userRoutes = require('./routes/user');
 
@@ -11,6 +13,7 @@ const userRoutes = require('./routes/user');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use(userRoutes); 
