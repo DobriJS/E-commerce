99edc3f977223112841baf5ead7ad74c2d161d76
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-09-05 18:02:27 +0300

    next commit

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// import routes
+const userRoutes = require('./routes/user');
+
+// routes middleware
+app.use(userRoutes); 
+
+
+// db
+const uri = 'mongodb://Dobri:Dobri@cluster0-shard-00-00.z1sur.mongodb.net:27017,cluster0-shard-00-01.z1sur.mongodb.net:27017,cluster0-shard-00-02.z1sur.mongodb.net:27017/<Dobri>?ssl=true&replicaSet=atlas-ok283v-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+ mongoose.connect(uri, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log(err))
+    
+    const port =  8000;
+    
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+
+
+    
+
+
+
+
+
+
