9ede0118e6086a8bfe132d604c44abd3225b52ac
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-09-06 10:54:59 +0300

    add requireSignin

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -49,4 +49,10 @@ exports.signin = (req, res) => {
 exports.signout = (req, res) => {
     res.clearCookie('t');
     res.json({ message: 'Signout success' });
-}
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+    userProperty: 'auth'
+});
