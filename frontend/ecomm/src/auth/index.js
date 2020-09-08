a3f5f2a5a77e2ed6862ec21831d4d630e0445106
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-09-08 14:15:19 +0300

    Show and hide signin and signout links

--- frontend/ecomm/src/auth/index.js
+++ frontend/ecomm/src/auth/index.js
@@ -47,3 +47,15 @@ export const signup = user => {
         .catch(err => console.log(err));
     }
   };
+
+  export const isAuthenticated = () => {
+      if(typeof window == 'undefined') {
+          return false;
+      }
+
+      if(localStorage.getItem('jwt')) {
+          return JSON.parse(localStorage.getItem('jwt'))
+      } else {
+          return false;
+      }
+  }
