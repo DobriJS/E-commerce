8ece9cb948171dbf7f4ab336c01a54a6bbcf3e51
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-09-12 13:54:54 +0300

    Create Cart component

--- frontend/ecomm/src/core/Card.js
+++ frontend/ecomm/src/core/Card.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
-import { addItem, updateItem, removeItem } from './cartHelpers'
+import { addItem, updateItem, removeItem } from './cartHelpers';
  
 const Card = ({ product, showViewProductButton = true, showAddToCartButton = true, cartUpdate = false,
     showRemoveProductButton = false,  setRun = f => f,
