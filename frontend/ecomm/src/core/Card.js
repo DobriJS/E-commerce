0d65f0111498ce1bab89ecd130235ab5907996fd
DobriJS <53184157+DobriJS@users.noreply.github.com>
2020-09-27 16:02:15 +0300

    Fix Code Format

--- frontend/ecomm/src/core/Card.js
+++ frontend/ecomm/src/core/Card.js
@@ -45,7 +45,8 @@ const Card = ({
     const showAddtoCartBtn = showAddToCartButton => {
         return (
           showAddToCartButton && (
-            <button onClick={addToCart} className='btn btn-outline-warning mt-2 mb-2 card-btn-1'>
+            <button onClick={addToCart} 
+            className='btn btn-outline-warning mt-0 mb-2 card-btn-1'>
                     Add to card
             </button>
           )
@@ -88,7 +89,7 @@ const Card = ({
                 removeItem(product._id);
                 setRun(!run);
               }}
-              className="btn btn-outline-danger mt-2 mb-2"
+              className="btn btn-outline-danger mt-0 mb-2"
                 
                 >
               Remove Product
@@ -99,23 +100,18 @@ const Card = ({
 
       return (
         
-              <div className='card'>
-              <div className='card-header name'>{product.name}</div>
-              <div className='card-body'>
-                      {shoudlRedirect(redirect)}
+      <div className='card'>
+        <div className='card-header card-header-1'>{product.name}</div>
+            <div className='card-body'>
+                  {shoudlRedirect(redirect)}
                   <ShowImage item={product} url="product" />
   
-                  <p className="card-p  mt-2">
-                      {product.description.substring(0, 100)}
-                  </p>
+          <p className="card-p mt-2">{product.description.substring(0, 100)}</p>
   
-                  <p className="card-p black-10">${product.price}</p>
-                  <p className='black-9'>
-                      Category: {product.category && product.category.name}
-                  </p>
-                  <p className='black-8'>
-                      Added on {moment(product.createdAt).fromNow()}
-                  </p>
+            <p className="card-p black-10">${product.price}</p>
+            <p className='black-9'>Category: {product.category && product.category.name}</p>
+                  
+            <p className='black-8'>Added on {moment(product.createdAt).fromNow()} </p>
   
                      {showStock(product.quantity)}
                      <br/>
