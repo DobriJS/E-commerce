e5d9b03eaca6dd4d33693a7719eb3dcbcc6b1415
DobriJS <53184157+DobriJS@users.noreply.github.com>
2020-09-27 16:34:58 +0300

    First Deploy

--- frontend/ecomm/src/user/UserDashboard.js
+++ frontend/ecomm/src/user/UserDashboard.js
@@ -75,15 +75,8 @@ const Dashboard = () => {
                                         return (
                                             <div key={i}>
                                                 <h6>Product name: {p.name}</h6>
-                                                <h6>
-                                                    Product price: ${p.price}
-                                                </h6>
-                                                <h6>
-                                                    Purchased date:{" "}
-                                                    {moment(
-                                                        p.createdAt
-                                                    ).fromNow()}
-                                                </h6>
+                                                <h6>Product price: ${p.price}</h6>
+                                                <h6>Purchased date:{" "} {moment(p.createdAt).fromNow()}</h6>
                                             </div>
                                         );
                                     })}
@@ -95,6 +88,13 @@ const Dashboard = () => {
             </div>
         );
     };
+                                                       
+                                                    
+                                                
+                                                    
+                                                    
+                                                
+                                                    
 
     return (
         <Layout
