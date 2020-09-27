25f5aeb8085d4f45d484823f4d24ed9d14f74d5a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2020-09-27 20:44:27 +0300

    Modify mongoose connection

--- backend/app.js
+++ backend/app.js
@@ -31,30 +31,15 @@ app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
 app.use("/api", orderRoutes);
 
-
-
-// db
-const uri = 'mongodb://Dobri:Dobri@cluster0-shard-00-00.z1sur.mongodb.net:27017,cluster0-shard-00-01.z1sur.mongodb.net:27017,cluster0-shard-00-02.z1sur.mongodb.net:27017/<Dobri>?ssl=true&replicaSet=atlas-ok283v-shard-0&authSource=admin&retryWrites=true&w=majority';
-
- mongoose.connect(uri, {
+ mongoose.connect(process.env.DATABASE, {
 
     useNewUrlParser: true,
     useUnifiedTopology: true
     })
-    .then(() => console.log('DB Connected'))
-    .catch(err => console.log(err))
+    .then(() => console.log('DB Connected'));
     
-    const port =  8000;
+    const port = process.env.PORT || 8000;
     
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
-    })
-
-
-    
-
-
-
-
-
-
+    });
